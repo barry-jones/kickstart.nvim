9c9701ff937efbdd758f8db96ef98bedcc2be789
Barry Jones <github@barryjones.me.uk>
2024-06-25 21:57:03 +0100

    added omnisharp lsp

--- init.lua
+++ init.lua
@@ -227,6 +227,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'OmniSharp/omnisharp-vim',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -577,6 +578,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`tsserver`) will work just fine
         -- tsserver = {},
         --
+        omnisharp = {},
 
         lua_ls = {
           -- cmd = {...},
